226d57bfd6952b1bdd3db6a07ffed3b7b20ac1c9
Anton Punith <antonpunith@gmail.com>
2018-07-21 13:10:03 +1000

    - tweaks to init script - starting script for react-native-app-auth

--- rn-init.sh
+++ rn-init.sh
@@ -4,11 +4,11 @@ PROJECT_NAME="$1"
 react-native init $PROJECT_NAME
 cd $PROJECT_NAME
 git init
-echo -e package-lock.json >> .gitignore
-echo -e yarn.lock >> .gitignore
+echo package-lock.json >> .gitignore
+echo yarn.lock >> .gitignore
 git add .
 git commit -m "initialising project"
 yarn add eslint-config-rallycoding --dev
-echo -e "{\n  \"extends\": \"rallycoding\",\n  \"parserOptions\": {\n    \"ecmaVersion\": 6,\n    \"ecmaFeatures\": {\n      \"arrowFunctions\": true\n    }\n  }\n}" >> .eslintrc
+echo "{\n  \"extends\": \"rallycoding\",\n  \"parserOptions\": {\n    \"ecmaVersion\": 6,\n    \"ecmaFeatures\": {\n      \"arrowFunctions\": true\n    }\n  }\n}" >> .eslintrc
 git add .
 git commit -m "adding eslint"
